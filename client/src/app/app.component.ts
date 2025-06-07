f9d59c35297600a84447a83589515f8e0cf94ea1
klos02 <bartoszklosinski02@gmail.com>
2025-06-07 20:02:34 +0200

    add register functionality to client app

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,43 +1,27 @@
-import { NgFor } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavComponent } from "./nav/nav.component";
+import { NavComponent } from './nav/nav.component';
 import { AccountService } from './_services/account.service';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavComponent],
+  imports: [RouterOutlet, NavComponent, HomeComponent],
   templateUrl: './app.component.html',
   standalone: true,
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent implements OnInit {
-  
   private accountService = inject(AccountService);
-  http = inject(HttpClient);
-  title = 'SocialApp';
-  users: any;
- 
+
   ngOnInit(): void {
-    this.getUsers();
     this.setCurrentUser();
   }
 
-  setCurrentUser(){
+  setCurrentUser() {
     const userString = localStorage.getItem('user');
     if (!userString) return;
     const user = JSON.parse(userString);
     this.accountService.currentUser.set(user);
   }
-
-  getUsers(){
-    this.http.get('https://localhost:7005/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),
-      complete: () => console.log('Request has completed')
-    })
-  }
-
-
 }
---
+++ client/src/app/home/home.component.ts
@@ -0,0 +1,33 @@
+import { Component, inject } from '@angular/core';
+import { RegisterComponent } from '../register/register.component';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-home',
+  imports: [RegisterComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
+  standalone: true,
+})
+export class HomeComponent {
+  
+  registerMode = false;
+  users: any;
+  http = inject(HttpClient);
+
+  ngOnInit(){
+    this.getUsers();
+  }
+
+  registerToggle() {
+    this.registerMode = !this.registerMode;
+  }
+
+  getUsers() {
+    this.http.get('https://localhost:7005/api/users').subscribe({
+      next: (response) => (this.users = response),
+      error: (error) => console.log(error),
+      complete: () => console.log('Request has completed'),
+    });
+  }
+}
